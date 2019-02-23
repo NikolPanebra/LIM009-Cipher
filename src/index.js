8e137c498f0331cd9eced538379139614ce1dc2d
nnkkll <nikol.panebra@gmail.com>
2019-02-23 17:29:26 -0500

    cambiar htmls a index1

--- src/index.js
+++ src/index.js
@@ -3,17 +3,25 @@
 /* DECLARAR Variables*/
 const mensaje2A = document.getElementById('mensaje2A');
 const button2A = document.getElementById('button2A');
-const mensaje2B = document.getElementById('mensaje2B')
-const button2B = document.getElementById('button2B')
+const mensaje2B = document.getElementById('mensaje2B');
+const button2B = document.getElementById('button2B');
+
  /* Funciones básicas */
  
  /*Capturar el mensaje si el usuario decide encriptar*/
-button2A.addEventListener('click',capturaParaEncriptar);
+ 
+ button2A.addEventListener('click', capturaParaEncriptar);
+ function capturaParaEncriptar() {
 
-function capturaParaEncriptar() {
 console.log(`${mensaje2A.value}`);
 } 
- /*Capturar el mensaje si el usuario decide desencriptar*/
- button2B.addEventListener('click',capturaParaDesencriptado);
 
- function capturaParDesencriptar()
+
+ /*Capturar el mensaje si el usuario decide desencriptar PONER UN IF PARA CONDICIONAR QUÉ ACCION TENDRÁ AL HACER CLICK EN EL BOTÓN*/
+
+ button2B.addEventListener('click',capturaParaDesencriptar);
+
+ function capturaParDesencriptar() {
+
+console.log(`${mensaje2B.value}`);
+ }
